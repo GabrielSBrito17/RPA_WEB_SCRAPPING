d4010169a831856cdd9d56387dd0152e8e722458
GabrielSBrito17 <89146836+GabrielSBrito17@users.noreply.github.com>
2021-08-18 13:37:45 -0300

    Versão 1.1

--- Projeto_RPA/API.py
+++ Projeto_RPA/API.py
@@ -4,16 +4,14 @@ import os
 
 app = Flask(__name__)
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(BASE_DIR)
-with open(BASE_DIR + '\\notebooks.json') as f:    
+with open(BASE_DIR + '\\Projeto_RPA\\notebooks.json') as f:    
     notebooks = json.load(f)
 
 @app.route('/notebooks', methods=['GET'])
 def home():
     return jsonify(notebooks), 200
-
 
 @app.route('/notebooks/<string:Nome>', methods=['GET'])
 def notebooks_per_Nome(Nome):
